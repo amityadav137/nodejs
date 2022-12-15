695f5ad36bd28a410d2488026ee812f27e813a2e
Amit Yadav <119371202+amityadav137@users.noreply.github.com>
2022-12-15 12:37:13 -0600

    Create use_my_math.js

---
+++ use_my_math.js
@@ -0,0 +1,7 @@
+const cal = require("./my_math");
+var x=3,y=5;
+console.log("3 + 5 = " + cal.add(x, y));
+console.log("3 - 5 = " + cal.subtract(x, y));
+console.log("3 * 5 = " + cal.multiply(x, y));
+console.log("3 / 5 = " + cal.divide(x, y));
+console.log("PI = " + cal.pi);
